0775eeac670f3455f4d7fef3e13b20337e6160f9
meghaa <meghaa@gmail.com>
2022-06-15 11:17:35 +0530

    third commit

--- src/com/cg/demo/Hello.java
+++ src/com/cg/demo/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Welcome to Capgemini!!");
+		System.out.println("Thankyou");
 
 	}
 
